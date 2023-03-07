e1ca97253f0fa2bf1ec80207ba25bc86fa54a077
KZ <zeng.kuriakin@gmail.com>
2023-03-07 16:54:50 +0800

    Passing Multiplier3 tests

--- Q2/test/test.js
+++ Q2/test/test.js
@@ -30,7 +30,7 @@ describe("HelloWorld", function () {
 
         const witness = await circuit.calculateWitness(INPUT, true);
 
-        //console.log(witness);
+        console.log(witness);
 
         assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
         assert(Fr.eq(Fr.e(witness[1]), Fr.e(6)));
@@ -73,23 +73,55 @@ describe("HelloWorld", function () {
 describe("Multiplier3 with Groth16", function () {
 
     beforeEach(async function () {
-        //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("GrothMultiplier3Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Circuit should multiply three numbers correctly", async function () {
-        //[assignment] insert your script here
+        const circuit = await wasm_tester("contracts/circuits/Multiplier3.circom");
+
+        const INPUT = {
+            "a": 2,
+            "b": 3,
+            "c": 4
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        // console.log(witness);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(24)));
     });
 
     it("Should return true for correct proof", async function () {
-        //[assignment] insert your script here
+        const { proof, publicSignals } = await groth16.fullProve({ "a": "2", "b": "3", "c": "4" }, "contracts/circuits/GrothMultiplier3/Multiplier3_js/Multiplier3.wasm", "contracts/circuits/GrothMultiplier3/circuit_final.zkey");
+        console.log('2x3x4 =', publicSignals[0]);
+        const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
+
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+        console.log(argv);
+
+        const a = [argv[0], argv[1]];
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+        const c = [argv[6], argv[7]];
+        const Input = argv.slice(8);
+
+        // Put the inputs and output, and proof into the verifier contract
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
     });
 
     it("Should return false for invalid proof", async function () {
-        //[assignment] insert your script here
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0]
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
     });
 });
 
-
+// The below is exactly the same
 describe("Multiplier3 with PLONK", function () {
 
     beforeEach(async function () {
